abb603b8f8051c28144be989ab62125a8ee0aef3
ndtands <ndtan.hcm@gmail.com>
2022-06-14 21:59:19 +0700

    change data warehouse

--- ml_core/test/data-extraction.py
+++ ml_core/test/data-extraction.py
@@ -11,6 +11,6 @@ if __name__ == '__main__':
     MONGODB_URL = f'mongodb+srv://{MONGODB_USER}:{MONGODB_PASSWORD}@cluster0.vk4zird.mongodb.net/test'
     client = MongoClient(MONGODB_URL)
     db=client[DATASOURCE]
-    collection_post = db.data
+    collection_post = db.post
     df = data_extraction.extract_data(project='mlops', collection=collection_post)
     logger.info("Data extract Done")
